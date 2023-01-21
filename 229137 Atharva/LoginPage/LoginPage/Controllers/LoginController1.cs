1c2618667360fffa7ae94bce54f0271ef1d2cf59
SurajK2025 <suraj.khokrale@rediffmail.com>
2023-01-21 15:43:41 +0530

    t

---
+++ 229137 Atharva/LoginPage/LoginPage/Controllers/LoginController1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LoginPage.Models;
+namespace LoginPage.Controllers
+{
+    public class LoginController1 : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public List<UserModel> Putvalue()
+        {
+            var user = new List<UserModel>()
+            {
+             new UserModel(1,"xyz","okay"),
+             new UserModel(2,"lmn","okay1"),
+             new UserModel(3,"opq","1234")
+
+            };
+        return user;
+        }
+        int attempt = 0;
+        public IActionResult Verify(UserModel usr)
+        {
+            
+            var u= Putvalue();
+            var ui=u.Where(u=>u.Id.Equals(usr.Id));
+            var up=u.Where(u=>u.Password.Equals(usr.Password));
+            var un=u.Where(u=>u.UserName.Equals(usr.UserName));
+
+            if (u.Count == 1)
+            {
+                ViewBag.message("Logged in Sucessfully");
+                return View(usr);
+            }
+
+            else
+            {
+                ViewBag.message("Login failed");
+                attempt++;
+                if (attempt > 2)
+                {
+                    ViewBag.message("Your account has been deactivated please constact administrator");
+                }
+                
+                return View();
+               
+            }
+
+            
+  
+        }
+        
+    }
+}
---
+++ 229137 Atharva/LoginPage/LoginPage/Models/UserModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        //default constructor
+        public UserModel()
+        {
+           this.Id = 100;
+            this.UserName = "userName";
+            this.Password = "password";
+        }
+
+       // parameterized constructor
+       public UserModel(int id, string userName, string password)
+        {
+            Id = id;
+            UserName = userName;
+            Password = password;
+       }
+
+     
+
+
+    }
+
